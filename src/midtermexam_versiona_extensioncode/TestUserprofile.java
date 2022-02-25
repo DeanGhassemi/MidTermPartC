67ae8d5424f2598831b7380b13a8355ff9147772
Dean <deang467@gmail.com>
2022-02-25 10:27:58 -0500

    third commit

--- src/midtermexam_versiona_extensioncode/TestUserprofile.java
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -3,13 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package midtermexam_versiona_extensioncode;
-
+import java.util.*;
 /**
  *
  * @author deang
  */
 public class TestUserprofile {
     public static void main(String[] arg){
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = input.next();
+        System.out.println("What is your favourite genre from these?");
+        System.out.println("Comedy, Drama, Action, Mystery");
+        String genre = input.next();
+        UserProfile user = new UserProfile(name, genre);
+        System.out.println("The user profile was created");
     }
 }
